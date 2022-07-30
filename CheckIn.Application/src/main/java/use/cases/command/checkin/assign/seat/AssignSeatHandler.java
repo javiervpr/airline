e460e12e5fcb9b6c1fce3051a25200d171dd876a
javiervpr <javiervacapereiraroca@gmail.com>
2022-07-30 06:35:42 -0400

    Update build

--- CheckIn.Application/src/main/java/use/cases/command/checkin/assign/seat/AssignSeatHandler.java
+++ CheckIn.Application/src/main/java/use/cases/command/checkin/assign/seat/AssignSeatHandler.java
@@ -1,10 +1,6 @@
 package use.cases.command.checkin.assign.seat;
 
 import an.awesome.pipelinr.Command;
-import factories.check.in.CheckInFactory;
-import factories.check.in.CreateCheckIn;
-import factories.seat.CreateSeat;
-import factories.seat.SeatFactory;
 import java.util.UUID;
 import model.CheckIn;
 import org.springframework.stereotype.Component;
@@ -17,10 +13,6 @@ public class AssignSeatHandler
   implements Command.Handler<AssignSeatCommand, UUID> {
 
   private final CheckInRepository checkInRepository;
-  private final PassangerRepository passangerRepository;
-  private final SeatRepository seatRepository;
-  private final CheckInFactory checkInFactory;
-  private final SeatFactory seatFactory;
 
   public AssignSeatHandler(
     CheckInRepository checkInRepository,
@@ -28,10 +20,6 @@ public class AssignSeatHandler
     SeatRepository seatRepository
   ) {
     this.checkInRepository = checkInRepository;
-    this.passangerRepository = passangerRepository;
-    this.seatRepository = seatRepository;
-    this.checkInFactory = new CreateCheckIn();
-    this.seatFactory = new CreateSeat();
   }
 
   @Override
@@ -45,7 +33,6 @@ public class AssignSeatHandler
       checkInRepository.update(checkIn);
       return checkIn.getId();
     } catch (Exception e) {
-      e.printStackTrace();
       return null;
     }
   }
